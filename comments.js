00dfd321850b7e85afb01539ebf644c399a97118
Akash Prajapati <125987351+akash5543@users.noreply.github.com>
2025-01-29 19:24:35 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,5 +1,2 @@
 // create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-console.log('Server is running on port 3000');
+console.log("HServer is running on port 3000");
