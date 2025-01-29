dff3e91505be005083216e557a944582b6eb0250
Akash Prajapati <125987351+akash5543@users.noreply.github.com>
2025-01-29 19:21:02 +0000

    Fix server code

--- comments.js
+++ comments.js
@@ -2,11 +2,4 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-// create route to get comments
-app.get('/comments', (req, res) => {
-    res.send('Comments');
-});
+console.log('Server is running on port 3000');
