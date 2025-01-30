db90081d4a500680fd76d8fda05b6a9586bbb1ef
Akash Prajapati <125987351+akash5543@users.noreply.github.com>
2025-01-30 18:44:36 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,44 +1,38 @@
 // create web server
 var express = require('express');
-var app = express();
-var fs = require('fs');
 var bodyParser = require('body-parser');
-var comments = require('./comments.json');
+var app = express();
+
+// use middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// create route to get comments
+// create a variable to store comments
+var comments = [
+  { name: 'John', message: 'Hello' },
+  { name: 'Jane', message: 'Hi' }
+];
+
+// create a route to get comments
 app.get('/comments', function(req, res) {
-    res.json(comments);
+  res.json(comments);
 });
 
-// create route to post comments
+// create a route to post comments
 app.post('/comments', function(req, res) {
-    comments.push(req.body);
-    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
-        console.log(err);
-    });
-    res.json(comments);
+  var comment = req.body;
+  comments.push(comment);
+  res.json(comments);
 });
 
-// create route to delete comments
-app.delete('/comments', function(req, res) {
-    comments = [];
-    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
-        console.log(err);
-    });
-    res.json(comments);
+// create a route to delete comments
+app.delete('/comments/:index', function(req, res) {
+  var index = req.params.index;
+  comments.splice(index, 1);
+  res.json(comments);
 });
 
-// create route to delete comment by id
-app.delete('/comments/:id', function(req, res) {
-    comments.splice(req.params.id, 1);
-    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
-        console.log(err);
-    });
-    res.json(comments);
-});
-
+// listen to port 3000
 app.listen(3000, function() {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
